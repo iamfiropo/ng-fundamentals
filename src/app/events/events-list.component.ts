f2224907769f4f65bbdd0f49c73c0c989e678908
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-10-01 15:49:10 +0100

    using template variables to interact with child components

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core'
     <div>
     <h1>Upcoming Angular Events</h1>
     <hr />
-    <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1">
+    <event-thumbnail #thumbnail [event]="event1">
     </event-thumbnail>
+    <h3>{{thumbnail.someProperty}}</h3>
+    <button class="btn btn-primary" (click)="thumbnail.logFoo()">
+      Log me some foo
+    </button>
     </div>
   `
 })
