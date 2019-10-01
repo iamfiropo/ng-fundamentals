638cac7fe12db60d170a922e1aea5a3eecdd2788
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-10-01 15:26:01 +0100

    child component communicating with parent component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core'
     <div>
     <h1>Upcoming Angular Events</h1>
     <hr />
-    <event-thumbnail [event]="event1"></event-thumbnail>
+    <event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1">
+    </event-thumbnail>
     </div>
   `
 })
@@ -23,5 +24,9 @@ export class EventsListComponent {
       city: 'London',
       country: 'England'
     }
+  }
+
+  handleEventClicked(data) {
+    console.log('received:', data)
   }
 }
